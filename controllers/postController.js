8509849516b509e3bf2aaf9ba6b951387475d644
Aaron Bermudez <aarondavidb31@gmail.com>
2022-11-28 13:27:52 -0500

    refactored the post model and added the controller file with CRUD, still missing the getPost function will update on it later in the day

---
+++ controllers/postController.js
@@ -0,0 +1,51 @@
+const Post = require("../models/postModel");
+
+// create post
+const createPost = async (req, res) => {
+    
+    const newPost = new Post(req.body);
+
+    try {
+        const savePost = await newPost.save();
+        res.status(200).json({savePost});
+    } catch (error){
+        res.status(400).json({error: error.message});
+    }
+};
+
+// edit post
+const editPost = async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.author === req.body.author){
+            await post.updateOne({$set:req.body});
+            res.status(200).json("updated!")
+        }else{
+            res.status(403).json("you can update only your post");
+        }
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+};
+
+const deletePost = async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.author === req.body.author){
+            await post.deleteOne();
+            res.status(200).json("deleted")
+        }else{
+            res.status(403).json("you can delete only your post");
+        }
+    } catch(error){
+        res.status(400).json({error: error.message})
+    } 
+};
+
+module.exports = {
+    createPost,
+    editPost,
+    deletePost,
+    //getPost,
+    //getPost,
+};
--- routes/post.js
+++ routes/post.js
@@ -1,50 +1,23 @@
-const router = require("express").Router();
-const Post = require("../models/postModel")
+const express = require("express");
 
-/* controller functions
 const {
     createPost,
-} = require("../controllers/postController");*/
+    editPost,
+    deletePost,
+    getPost,
+} = require("../controllers/postController");
 
+const router = express.Router();
 // create post
-router.post("/", async (req, res) => {
-    const newPost = new Post(req.body);
-
-    try {
-        const savePost = await newPost.save();
-        res.status(200).json({savePost});
-    } catch (error){
-        res.status(400).json({error: error.message});
-    }
-})
+router.post("", createPost);
 
 // edit post
-router.put("/:id", async (req, res) => {
-    try{
-        const post = await Post.findById(req.params.id);
-        if(post.author === req.body.author){
-            await post.updateOne({$set:req.body});
-            res.status(200).json("updated!")
-        }else{
-            res.status(403).json("you can update only your post");
-        }
-    } catch(error){
-        res.status(400).json({error: error.message})
-    }
-})
+router.put("/:id", editPost);
 
 // delete post
-router.delete("/:id", async (req, res) => {
-    try{
-        const post = await Post.findById(req.params.id);
-        if(post.author === req.body.author){
-            await post.deleteOne();
-            res.status(200).json("deleted")
-        }else{
-            res.status(403).json("you can delete only your post");
-        }
-    } catch(error){
-        res.status(400).json({error: error.message})
-    } 
-})
+router.delete("/:id", deletePost);
+
+// get post
+//router.get(":/id", getPost);
+
 module.exports = router;
