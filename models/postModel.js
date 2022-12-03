611f9e0c1221d2f9a1c81c16c1ba1c400df71156
Aaron Bermudez <aarondavidb31@gmail.com>
2022-12-03 13:55:07 -0500

    Refactored post model and now the getPost and createPost returnthe user, the json still shows their userId in mongoDB yet, the functions themselves return the object

--- models/postModel.js
+++ models/postModel.js
@@ -5,35 +5,54 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema(
   {
-    body: {
-      type: String,
-      required: true,
+    // id: {
+    //   type: String,
+    //   required: true,
+    // },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
     },
     image: {
       type: String,
-      required: false,
     },
-    likes: {
+    caption: {
+      type: String
+    },
+    liked: {
+      type: Boolean,
+      default: false
+    },
+    comments: {
       type: Array,
-      default: [],
+      default: []
     },
-    date: {
+    createdAt: {
       type: Date,
       default: Date.now,
       required: true,
-    },
-    author: {
-      type: String,
-      require: true,
-    },
-    mentions: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    comments: {
-      body: "string",
-      by: mongoose.Schema.Types.ObjectId,
-    },
+    }
+    // likes: {
+    //   type: Array,
+    //   default: [],
+    // },
+    // date: {
+    //   type: Date,
+    //   default: Date.now,
+    //   required: true,
+    // },
+    // author: {
+    //   type: String,
+    //   require: true,
+    // },
+    // mentions: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "User",
+    // },
+    // comments: {
+    //   body: "string",
+    //   by: mongoose.Schema.Types.ObjectId,
+    // },
   },
   { timestamps: true }
 );
